0dc17eaeb3145be03022751f6729f6cf0ea6e49e
Arthur Michajlenko <michajlenko1967@gmail.com>
2019-11-12 16:35:42 +0200

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,26 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-// void main() => runApp(MyApp());
-Database db1;
+import 'order_detail.dart';
+//* uncomment when begin using models
+// import 'models/orders.dart';
+// import 'models/couriers.dart';
+// import 'models/clients.dart';
+
+// void main() => runApp(GelibertApp());
+Database db;
 void main() async {
+  await _workdb();
+
+  runApp(GelibertApp());
+}
+
+Future _workdb() async {
   var databasePath = await getDatabasesPath();
   var path = join(databasePath, "gelibert.db");
   var exists = await databaseExists(path);
   if (!exists) {
-    print("Creating from assets");
+    // Creating database from assets
     try {
       await Directory(dirname(path)).create(recursive: true);
     } catch (_) {}
@@ -22,119 +34,89 @@ void main() async {
     List<int> bytes =
         data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
     await File(path).writeAsBytes(bytes, flush: true);
-  } else {
-    print("Opening exsisting database");
   }
   // Open the database
-  final db1 = await openDatabase(path);
-  // print(db.path);
-  // print(db.isOpen);
-  // db1 = db;
-  print(db1.isOpen);
-  print(db1.path);
-  // print(db.toString());
-  print(db1.toString());
-
-  runApp(MyApp());
+  final db = await openDatabase(path);
+  //* remove when begin using db
+  print(db);
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class GelibertApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Orders',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Gelibert'),
+      home: OrdersPage(title: 'Orders'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class OrdersPage extends StatefulWidget {
+  OrdersPage({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _OrdersPageState createState() => _OrdersPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _OrdersPageState extends State<OrdersPage> {
   int _counter = 0;
+
+  @override
+  void dispose() {
+    db.close();
+    super.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: ListView.builder(
           itemCount: 3,
           itemBuilder: (context, index) {
-            return ListTile(
-              leading: GestureDetector(
-                behavior: HitTestBehavior.translucent,
-                onTap: () {
-                  launch("sms:+37369130450");
-                },
-                child: Container(
-                  width: 48,
-                  height: 48,
-                  padding: EdgeInsets.symmetric(vertical: 4.0),
-                  alignment: Alignment.center,
-                  child: CircleAvatar(),
+            return Card(
+              child: ListTile(
+                leading: GestureDetector(
+                  behavior: HitTestBehavior.translucent,
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => OrderDetail()));
+                  },
+                  child: Container(
+                    width: 48,
+                    height: 48,
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    alignment: Alignment.center,
+                    child: CircleAvatar(
+                      child: Icon(Icons.shopping_cart),
+                    ),
+                  ),
                 ),
+                title: Text('Curier: ' + '$_counter'),
+                onTap: () => launch("tel:+3736913040"),
+                dense: false,
+                enabled: true,
               ),
-              title: Text('Curier: ' + '$_counter'),
-              onTap: () => launch("tel:+3736913040"),
-              dense: false,
-              enabled: true,
             );
           }),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
+
