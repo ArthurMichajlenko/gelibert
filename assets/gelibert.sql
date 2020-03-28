91462a93dee21217ed68b5dade6e452df3e078d2
Arthur Michajlenko <michajlenko1967@gmail.com>
2020-03-29 01:18:04 +0200

    consists* add di field for List

--- assets/gelibert.sql
+++ assets/gelibert.sql
@@ -1,44 +1,46 @@
 CREATE TABLE IF NOT EXISTS "consists_from" (
-	"id"	INTEGER,
-	"product"	TEXT,
-	"quantity"	REAL,
-	"price"	REAL,
-	"ext_info" TEXT
+  "di" INTEGER NOT NULL UNIQUE,
+  "id" INTEGER,
+  "product" TEXT,
+  "quantity" REAL,
+  "price" REAL,
+  "ext_info" TEXT
 );
 CREATE TABLE IF NOT EXISTS "consists_to" (
-	"id"	INTEGER,
-	"product"	TEXT,
-	"quantity"	REAL,
-	"price"	REAL,
-	"ext_info" TEXT
+  "di" INTEGER NOT NULL UNIQUE,
+  "id" INTEGER,
+  "product" TEXT,
+  "quantity" REAL,
+  "price" REAL,
+  "ext_info" TEXT
 );
 CREATE TABLE IF NOT EXISTS "orders" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"courier_id"	INTEGER,
-	"client_id"	INTEGER,
-	"payment_method"	TEXT,
-	"order_cost"	REAL,
-	"delivered"	INTEGER,
-	"delivery_delay"	INTEGER,
-	"date_start"	TEXT,
-	"date_finish"	TEXT,
-	PRIMARY KEY("id")
+  "id" INTEGER NOT NULL UNIQUE,
+  "courier_id" INTEGER,
+  "client_id" INTEGER,
+  "payment_method" TEXT,
+  "order_cost" REAL,
+  "delivered" INTEGER,
+  "delivery_delay" INTEGER,
+  "date_start" TEXT,
+  "date_finish" TEXT,
+  PRIMARY KEY("id")
 );
 CREATE TABLE IF NOT EXISTS "clients" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"name"	TEXT,
-	"tel"	TEXT,
-	"address"	TEXT,
-	PRIMARY KEY("id")
+  "id" INTEGER NOT NULL UNIQUE,
+  "name" TEXT,
+  "tel" TEXT,
+  "address" TEXT,
+  PRIMARY KEY("id")
 );
 CREATE TABLE IF NOT EXISTS "couriers" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"imei"	INTEGER NOT NULL UNIQUE,
-	"tel"	TEXT,
-	"name"	TEXT,
-	"car_number"	TEXT,
-	"latitude"	REAL,
-	"longitude"	REAL,
-	"address"	TEXT,
-	PRIMARY KEY("id")
+  "id" INTEGER NOT NULL UNIQUE,
+  "imei" INTEGER NOT NULL UNIQUE,
+  "tel" TEXT,
+  "name" TEXT,
+  "car_number" TEXT,
+  "latitude" REAL,
+  "longitude" REAL,
+  "address" TEXT,
+  PRIMARY KEY("id")
 );
