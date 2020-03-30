fc8359c907eee05660265c981085d81a4c9d0bf3
Arthur Michajlenko <michajlenko1967@gmail.com>
2020-03-30 09:40:14 +0300

    Changes from Local

--- lib/consists_detail.dart
+++ lib/consists_detail.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'main.dart';
 import 'models/orders.dart';
 import 'package:barcode_scan/barcode_scan.dart';
@@ -15,7 +15,7 @@ class ConsistsDetail extends StatefulWidget {
 }
 
 class _ConsistsDetailState extends State<ConsistsDetail> {
-  String extInfo;
+  // String extInfo;
 
   @override
   Widget build(BuildContext context) {
@@ -79,30 +79,65 @@ class _ConsistsDetailState extends State<ConsistsDetail> {
                           child: Text(
                               '${widget.order.consistsTo[i].price}x${widget.order.consistsTo[i].quantity} шт.'),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: TextField(
-                            controller: TextEditingController(
-                              text: widget.order.consistsTo[i].extInfo,
-                            ),
-                            decoration: InputDecoration(
-                              contentPadding: EdgeInsets.symmetric(
-                                vertical: 1.0,
-                                horizontal: 4.0,
+                        if (widget.order.delivered == 1)
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(widget.order.consistsTo[i].extInfo),
+                          )
+                        else
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: TextField(
+                              onSubmitted: (text) async {
+                                widget.order.consistsTo[i].extInfo = text;
+                                await db.update(
+                                    'consists_to',
+                                    {
+                                      'ext_info':
+                                          widget.order.consistsTo[i].extInfo,
+                                    },
+                                    where: 'id=? AND di=?',
+                                    whereArgs: [
+                                      widget.order.id,
+                                      widget.order.consistsTo[i].di
+                                    ]);
+                              },
+                              controller: TextEditingController(
+                                text: widget.order.consistsTo[i].extInfo,
                               ),
-                              border: OutlineInputBorder(),
-                              labelText: 'Доп.инф (S/N...)',
-                              suffixIcon: GestureDetector(
-                                dragStartBehavior: DragStartBehavior.down,
-                                child: Icon(
-                                  Icons.camera_alt,
-                                  size: 40,
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.symmetric(
+                                  vertical: 1.0,
+                                  horizontal: 4.0,
                                 ),
-                                onTap: scan,
+                                border: OutlineInputBorder(),
+                                labelText: 'Доп.инф (S/N...)',
+                                suffixIcon: GestureDetector(
+                                  dragStartBehavior: DragStartBehavior.down,
+                                  child: Icon(
+                                    Icons.camera_alt,
+                                    size: 40,
+                                  ),
+                                  onTap: () async {
+                                    widget.order.consistsTo[i].extInfo =
+                                        await scan();
+                                    await db.update(
+                                        'consists_to',
+                                        {
+                                          'ext_info': widget
+                                              .order.consistsTo[i].extInfo,
+                                        },
+                                        where: 'id=? AND di=?',
+                                        whereArgs: [
+                                          widget.order.id,
+                                          widget.order.consistsTo[i].di
+                                        ]);
+                                    setState(() {});
+                                  },
+                                ),
                               ),
                             ),
-                          ),
-                        )
+                          )
                       ],
                     ),
                     trailing: Text('Кол-во: \n' +
@@ -133,29 +168,69 @@ class _ConsistsDetailState extends State<ConsistsDetail> {
                           '. ' +
                           widget.order.consistsFrom[i].product,
                     ),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextField(
-                        controller: TextEditingController(
-                          text: widget.order.consistsFrom[i].extInfo,
-                        ),
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.symmetric(
-                            vertical: 1.0,
-                            horizontal: 4.0,
-                          ),
-                          border: OutlineInputBorder(),
-                          labelText: 'Доп.инф (S/N...)',
-                          suffixIcon: GestureDetector(
-                            dragStartBehavior: DragStartBehavior.down,
-                            child: Icon(
-                              Icons.camera_alt,
-                              size: 40,
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        if (widget.order.delivered == 1)
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(widget.order.consistsFrom[i].extInfo),
+                          )
+                        else
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextField(
+                              onSubmitted: (text) async {
+                                widget.order.consistsFrom[i].extInfo = text;
+                                await db.update(
+                                    'consists_from',
+                                    {
+                                      'ext_info':
+                                          widget.order.consistsFrom[i].extInfo,
+                                    },
+                                    where: 'id=? AND di=?',
+                                    whereArgs: [
+                                      widget.order.id,
+                                      widget.order.consistsFrom[i].di
+                                    ]);
+                              },
+                              controller: TextEditingController(
+                                text: widget.order.consistsFrom[i].extInfo,
+                              ),
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.symmetric(
+                                  vertical: 1.0,
+                                  horizontal: 4.0,
+                                ),
+                                border: OutlineInputBorder(),
+                                labelText: 'Доп.инф (S/N...)',
+                                suffixIcon: GestureDetector(
+                                  dragStartBehavior: DragStartBehavior.down,
+                                  child: Icon(
+                                    Icons.camera_alt,
+                                    size: 40,
+                                  ),
+                                  onTap: () async {
+                                    widget.order.consistsFrom[i].extInfo =
+                                        await scan();
+                                    await db.update(
+                                        'consists_from',
+                                        {
+                                          'ext_info': widget
+                                              .order.consistsFrom[i].extInfo,
+                                        },
+                                        where: 'id=? AND di=?',
+                                        whereArgs: [
+                                          widget.order.id,
+                                          widget.order.consistsFrom[i].di
+                                        ]);
+                                    setState(() {});
+                                  },
+                                ),
+                              ),
                             ),
-                            onTap: scan,
                           ),
-                        ),
-                      ),
+                      ],
                     ),
                     trailing: Text('Кол-во: \n' +
                         widget.order.consistsFrom[i].quantity.toString() +
@@ -171,21 +246,12 @@ class _ConsistsDetailState extends State<ConsistsDetail> {
   }
 
   Future scan() async {
+    String extInfo;
     try {
       String code = await BarcodeScanner.scan();
-      setState(() => extInfo = code);
-    } on PlatformException catch (e) {
-      if (e.code == BarcodeScanner.CameraAccessDenied) {
-        setState(() => extInfo = 'Camera permission not granted');
-      } else {
-        setState(() => extInfo = 'Unknown error: $e');
-      }
-    } on FormatException {
-      setState(() => extInfo =
-          'null (User returned using the "back"-button before scanning anything)');
-    } catch (e) {
-      setState(() => extInfo = 'Unknown error: $e');
-    }
-    print(extInfo);
+      extInfo = code;
+    } catch (_) {}
+    // print(extInfo);
+    return extInfo;
   }
 }
