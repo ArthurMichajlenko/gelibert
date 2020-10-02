7e2c808ecd93a0c0358037aa196bfb014368cc94
Arthur Michajlenko <michajlenko1967@gmail.com>
2020-10-03 01:52:26 +0300

    After migration on New Flutter

--- android/app/src/main/kotlin/infinitloop/md/gilebert/MainActivity.kt
+++ android/app/src/main/kotlin/infinitloop/md/gilebert/MainActivity.kt
@@ -1,13 +1,14 @@
 package infinitloop.md.gilebert
 
-import android.os.Bundle
+// import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+// import io.flutter.app.FlutterActivity
+// import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+  // override fun onCreate(savedInstanceState: Bundle?) {
+  //   super.onCreate(savedInstanceState)
+  //   GeneratedPluginRegistrant.registerWith(this)
+  // }
 }
